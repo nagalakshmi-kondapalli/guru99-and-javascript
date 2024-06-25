aed161bb99d4bdf5c568f95215151f3700c8eb50
nagalakshmi-kondapalli <nagalakshmi.kondapalli@synlogica.com>
2024-06-25 12:18:21 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,8 +1,6 @@
 /* Write a Program to take a 2 digit number and calculate sum of digits. */
-var a = Number(prompt("Enter first number"));
-document.write("value a="+a)
-document.write("<br>");
-var b = Number(prompt("Enter second number"));
-document.write("value b="+b)
-document.write("<br>");
-document.write("The sum of two digit numbers are :" +(a + b)); 
+no = parseInt(prompt("Enter two digit number:"));
+        q = no/10;
+        r = no%10;
+        sum = q + r;
+        alert("double digit sum"+sum);
